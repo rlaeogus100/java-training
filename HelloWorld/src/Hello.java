84c3e76d26036e1624ad1b4d1be7ae95ad3248e1
rlaeogus <seo223kr@naver.com>
2018-08-25 23:24:16 +0900

    add HelloWold

---
+++ HelloWorld/src/Hello.java
@@ -0,0 +1,10 @@
+
+public class Hello {
+	    public static void main(String[] args){
+	      //TODO auto-generated method stub
+	      //sysout Ctrl + space 
+	    System.out.println("Hello World");
+	      //Save:Ctrl + s
+	      //Run:Ctrl + F11
+	    }
+}
