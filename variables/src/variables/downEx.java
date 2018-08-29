0b5c8afd57489abc0653f385643fbb8fbf8b83a9
rlaeogus <seo223kr@naver.com>
2018-08-29 23:20:13 +0900

    add oper

---
+++ variables/src/variables/downEx.java
@@ -0,0 +1,15 @@
+package variables;
+
+public class downEx {
+	public static void main(String[] args) {
+		//숫자의 경우 int
+		
+		byte value = 65;
+		char ch = (char) value;
+		
+		System.out.println(ch);
+		
+		
+				
+	}
+}
